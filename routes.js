1656d98673808f5b60fb6f56a609b203594737ba
Clay O'Dell <clay.odell.music@gmail.com>
2024-06-24 19:41:56 -0500

    added patch route

--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = new express.Router();
-
+const items = require("./fakeDb");
+const ExpressError = require("./expressError");
+const {errorHandler} = require("./middleware");
 
 router.get("/", (req, res) => {
   return res.json({ items: items });
@@ -11,7 +13,7 @@ router.get("/:name", (req, res) => {
     const item = items.find(i => i.name === name);
 
     if (!item) {
-        return res.status(404).json({ error: "Item not found"});
+        throw new ExpressError("Item not found", 404);
     }
     return res.status(200).json(item);
 });
@@ -24,3 +26,27 @@ router.post("/", (req, res) => {
 });
 
 module.exports = router;
+
+router.patch("/:name", async (req, res, next) => {
+    try {
+        const oldName = req.params.name;
+        const newName = req.body.name;
+        const newPrice = req.body.price;
+
+        let item = items.find(i => i.name === oldName);
+
+        if (!item) {
+            throw new ExpressError("Item not found", 404);
+        }
+        if (newName) {
+            item.name = newName;
+        }
+        if (newPrice) {
+            item.price = newPrice;
+        }
+        return res.json({ updated: item});
+    } catch (err) {
+        return next(err);
+    }
+});
+
